3e6773a39489a933bf8f84129199906fee576421
Matt Fleming <matt@codeblueprint.co.uk>
2016-05-03 20:46:54 +0100

    sched/fair: Update rq clock before updating nohz CPU load
    If we're accessing rq_clock() (e.g. in sched_avg_update()) we should
    update the rq clock before calling cpu_load_update(), otherwise any
    time calculations will be stale.
    
    All other paths currently call update_rq_clock().
    
    Change-Id: Ie22ffd44507b94c6925d685fafa392d5954070db
    Signed-off-by: Matt Fleming <matt@codeblueprint.co.uk>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Wanpeng Li <wanpeng.li@hotmail.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mel Gorman <mgorman@techsingularity.net>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Mike Galbraith <umgwanakikbuti@gmail.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Rik van Riel <riel@redhat.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/1462304814-11715-1-git-send-email-matt@codeblueprint.co.uk
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5188,6 +5188,7 @@ void update_cpu_load_nohz(void)
 		return;
 
 	raw_spin_lock(&this_rq->lock);
+	update_rq_clock(this_rq);
 	pending_updates = curr_jiffies - this_rq->last_load_update_tick;
 	if (pending_updates) {
 		this_rq->last_load_update_tick = curr_jiffies;
