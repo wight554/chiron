4547b266b95edb65efd6ada826bb7531bb6f76f7
Vol Zhdanov <wight554@gmail.com>
2018-06-02 02:31:12 +0000

    qcacld-3.0: fix misleading-indentation warnings

--- drivers/staging/qcacld-3.0/core/mac/src/cfg/cfg_proc_msg.c
+++ drivers/staging/qcacld-3.0/core/mac/src/cfg/cfg_proc_msg.c
@@ -1661,7 +1661,7 @@ static void proc_get_req(tpAniSirGlobal pMac, uint16_t length, uint32_t *pParam)
 	uint32_t *pValue;
 
 	pe_debug("Rcvd cfg get request %d bytes", length);
-	for (i = 0; i < length / 4; i++)
+	for (i = 0; i < length / 4; i++) {
 		pe_debug("[%2d] 0x%08x", i, pParam[i]);
 
 		if (!pMac->cfg.gCfgStatus) {
@@ -1730,6 +1730,7 @@ static void proc_get_req(tpAniSirGlobal pMac, uint16_t length, uint32_t *pParam)
 				length -= sizeof(uint32_t);
 			}
 		}
+	}
 
 } /*** end procGetReq() ***/
 
